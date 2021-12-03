0b9b19724b27d636dd72496310b020d49e08dedb
Yumika Shiba <yumika.shiba@mail.mcgill.ca>
2021-12-03 13:29:07 -0500

    Update combine_bam_fa.sh

--- bash/combine_bam_fa.sh
+++ bash/combine_bam_fa.sh
@@ -3,14 +3,15 @@
 mkdir ~/work/ae/867b940ddb7a3627d4b853c9f0b292/genotyping/IGV
 
 for d1 in */ ; do
+	cd ~/work/ae/867b940ddb7a3627d4b853c9f0b292/
 	samtools view -b mapped.sam > mapped.bam
 	samtools sort mapped.bam > mapped.sorted.bam
 	samtools index mapped.sorted.bam
-	mkdir ~/work/ae/867b940ddb7a3627d4b853c9f0b292/genotyping/"$d1"
-	mv mapped.sorted.bam ~/work/ae/867b940ddb7a3627d4b853c9f0b292/genotyping/IGV/"$d1"/"$("$d1" + "mapped.sorted.bam")"
+	mkdir ~/work/ae/867b940ddb7a3627d4b853c9f0b292/genotyping/$d1
+	mv mapped.sorted.bam ~/work/ae/867b940ddb7a3627d4b853c9f0b292/genotyping/IGV/$d1/"$("$d1" + "mapped.sorted.bam")"
 done
 
 for d2 in ~/work/ae/867b940ddb7a3627d4b853c9f0b292/genotyping/locusAlleles/; do
-	cd "$d2"
-	cp alleles.fa ~/work/ae/867b940ddb7a3627d4b853c9f0b292/genotyping/IGV/"$d2"/"$("$d2" + "alleles.fa")" 
+	cd ~/work/ae/867b940ddb7a3627d4b853c9f0b292/genotyping/locusAlleles/$d2
+	cp alleles.fa ~/work/ae/867b940ddb7a3627d4b853c9f0b292/genotyping/IGV/$d2/"$("$d2" + "alleles.fa")" 
 done
